cf89dc560b3ab21414b2897fd8a21f7b59cae612
Karina Lamoreux <karina.lamroeux@gmail.com>
2024-10-08 10:57:53 -0400

    update testing midpoint

--- midpoint_euler_test.m
+++ midpoint_euler_test.m
@@ -2,19 +2,20 @@ function midpoint_euler_test()
     t = 0;
     tspan = [0, 100];
     h_ref = 0.1;
-    X0 = 0;
+    X0 = 3;
     
     % solving step functions for the same point
     [XBm, ~] = explicit_midpoint_step(@rate_func01, t, X0, h_ref);
     [XBe, ~] = forward_euler_step(@rate_func01, t, X0, h_ref);
     
     % solving the analytical solution
-    sols = length(tspan);
-    ts = linspace(tspan(1), tspan(2));
+    num_points = 1000;
+    ts = linspace(tspan(1), tspan(2), num_points);
+    sols = length(ts);
     for i=1:length(ts)
         sols(i) = solution01(ts(i));
     end
-    
+
     [t_listm, X_listm, ~,~] = explicit_midpoint_fixed_step_integration(@rate_func01, tspan, X0, h_ref);
     [t_liste, X_liste, ~, ~] = forward_euler_fixed_step_integration(@rate_func01, tspan, X0, h_ref);
     clf; hold on;
